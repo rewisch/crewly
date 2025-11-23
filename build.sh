cf9f9e925e1d1b561fc2181fd57dd07c8eccdd4c
Reto Wietlisbach <rwietlisbach@protonmail.com>
2025-11-23 22:34:26 +0100

    add build.sh

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROD=false
+
+while getopts "p" flag; do
+  case "${flag}" in
+    p) PROD=true ;;
+  esac
+done
+
+if [ "$PROD" = true ]; then
+  echo "Building PRODUCTION version..."
+  gcc ./src/main.c \
+    -o ./dst/crewly \
+    -O2 \
+    -flto \
+    -s \
+    -DNDEBUG
+else
+  echo "Building DEVELOPMENT version..."
+  gcc ./src/main.c \
+    -o ./dst/crewly \
+    -g \
+    -Wall \
+    -Wextra
+fi
